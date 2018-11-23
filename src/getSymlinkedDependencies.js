bc18ec96b835a0082dcaae8a0cc24f317d6be4c3
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2018-11-23 18:11:32 +0100

    Add looking recursively for dependency in parent folders

--- src/getSymlinkedDependencies.js
+++ src/getSymlinkedDependencies.js
@@ -1,14 +1,32 @@
 const fs = require('fs')
+const path = require('path')
 
-const isSymlink = dependency =>
-    fs.lstatSync(`node_modules/${dependency}`).isSymbolicLink()
+const resolveDependency = dependency => {
+    if (fs.existsSync(dependency)) {
+        return path.resolve('.', dependency)
+    } else {
+        if (
+            path.resolve(path.join('..', dependency)) ===
+            path.resolve(dependency)
+        ) {
+            return null
+        } else {
+            return resolveDependency(path.join('..', dependency))
+        }
+    }
+}
+
+const isSymlink = dependency => {
+    const dependencyPath = resolveDependency(`node_modules/${dependency}`)
+    if (dependencyPath === null)
+        throw new Error(`Module ${module} is not installed.`)
+    return fs.lstatSync(dependencyPath).isSymbolicLink()
+}
 
 module.exports = directory => {
     const pacakgeJson = require(`${directory}/package.json`)
     return [
         ...Object.keys(pacakgeJson.devDependencies || {}),
         ...Object.keys(pacakgeJson.dependencies || {}),
-    ]
-        .filter(isSymlink)
-        .filter(dep => fs.existsSync(`node_modules/${dep}`))
+    ].filter(isSymlink)
 }
